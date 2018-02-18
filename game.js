8ad7aa05c0adbc382c863c0e823097dae6952720
Joao Aguiam <joaoaguiam@gmail.com>
2018-02-18 00:01:15 -0700

    Update missile coordinates

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 var sprites = {
  ship: { sx: 0, sy: 0, w: 37, h: 42, frames: 1 },
- missile: { sx: 0, sy: 30, w: 2, h: 10, frames: 1 },
+ missile: { sx: 200, sy: 0, w: 2, h: 10, frames: 1 },
  enemy_purple: { sx: 37, sy: 0, w: 42, h: 43, frames: 1 },
  enemy_bee: { sx: 79, sy: 0, w: 37, h: 43, frames: 1 },
  enemy_ship: { sx: 116, sy: 0, w: 42, h: 43, frames: 1 },
--- taiga-connector.js
+++ taiga-connector.js
@@ -1,3 +1,5 @@
+let web3 = window.web3;
+
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
